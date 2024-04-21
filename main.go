2b7e0feae2514b46074ad0febf72e0e750f9434f
Illia <illiadovho@gmail.com>
2024-04-21 20:27:00 +0100

    Added main.go comments

--- main.go
+++ main.go
@@ -11,24 +11,27 @@ func main() {
 	defer keyboard.Close()
 	// enable cursor after program finish
 	defer EnableCursor()
+	// thread to wait for keys
 	go readKeys(&char, &key, &keyBool)
 	// disable console cursor
-	if !DisableCursor() {
+	if DisableCursor() {
 		fmt.Print("Unfortunately we cannot disable your cursor blink. Press Enter to continue and Ctrl+Q to exit")
 		for {
-			_, key, _ := keyboard.GetKey()
-			if key == keyboard.KeyEnter {
-				break
-			} else if key == keyboard.KeyCtrlQ {
-				os.Exit(101)
+			if keyBool {
+				if key == keyboard.KeyEnter {
+					break
+				} else if key == keyboard.KeyCtrlQ {
+					os.Exit(101)
+				}
 			}
 		}
 	}
 
-	// enter game if user didn't exit
+	// program loop
 	contin := true
 	for contin {
 		if menu() {
+			// enter game if user didn't exit
 			contin = game()
 		} else {
 			contin = false
@@ -36,5 +39,4 @@ func main() {
 	}
 	ClearConsole()
 	blueFont.Print("\tThanks for choosing to play our Sudoku. May you have a blessed day :)")
-	//time.Sleep(time.Second * 5)
 }
